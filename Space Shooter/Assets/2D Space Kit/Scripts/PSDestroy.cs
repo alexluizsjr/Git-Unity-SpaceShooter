6696b72441ea191d482080feb7d44d7ca98c61ec
alexluizsjr <113055874+alexluizsjr@users.noreply.github.com>
2023-08-20 12:39:12 -0300

    upd initial project

---
+++ Space Shooter/Assets/2D Space Kit/Scripts/PSDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PSDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	ParticleSystem ps = GetComponent<ParticleSystem>();
+    Destroy(gameObject, ps.main.duration);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
